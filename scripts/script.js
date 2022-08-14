d054342a1310a48a9ab00b161ceb027aec56458e
Khaled Hegazy <khaledhegazy870@gmail.com>
2022-08-14 14:16:44 +0200

    adding next & back buttons feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 let data;
-let courseIdx = 0;
+let courseIdxArr = [];
 const getData = async (url) => {
     let response = await fetch(url);
     let json = await response.json();
     data = json;
     let topicsNav = document.querySelector(".topic-list");
     for (let i = 0; i < data.length; i++) {
+        courseIdxArr = [...courseIdxArr, 0];
         let topic = document.createElement("li");
         topic.setAttribute("class", "topic-elem nav-item");
         topic.setAttribute("role", "presentation");
@@ -19,7 +20,7 @@ const getData = async (url) => {
             topicLink.setAttribute("aria-selected", "false");
         }
 
-        topicLink.setAttribute("id", "tab-link" + data[i].id);
+        topicLink.setAttribute("id", "tab-link-" + data[i].id);
         topicLink.setAttribute("data-bs-toggle", "tab");
         topicLink.setAttribute("href", "#tab-content-" + data[i].id);
         topicLink.setAttribute("role", "tab");
@@ -43,114 +44,113 @@ const getData = async (url) => {
             "aria-labelledby",
             "tab-link" + data[i].id
         );
+        renderPage(data[i], topicContentTab);
 
-        let topic = document.createElement("div");
-        topic.setAttribute("class", "section-two-topic");
-
-        let subHeader = document.createElement("h2");
-        subHeader.setAttribute("class", "section-two-sub-header");
-        subHeader.textContent = data[i].content.header;
-        topic.appendChild(subHeader);
-
-        let subParagraph = document.createElement("p");
-        subParagraph.setAttribute("class", "section-two-sub-paragraph");
-        subParagraph.textContent = data[i].content.paragraph;
-        topic.appendChild(subParagraph);
-
-        topicContentTab.appendChild(topic);
-
-        let exploreBtn = document.createElement("input");
-        exploreBtn.setAttribute("class", "explore-btn");
-        exploreBtn.setAttribute("id", "explore-btn-" + data[i].id);
-        exploreBtn.setAttribute("type", "button");
-        exploreBtn.setAttribute("value", "Explore " + data[i].name);
-        topicContentTab.appendChild(exploreBtn);
-
-        let courseList = document.createElement("div");
-        courseList.setAttribute("class", "section-two-list");
-        for (let j = 0; j < data[i].content.courses.length; j++) {
-            let course = data[i].content.courses[j];
-            let article = document.createElement("artice");
-            article.setAttribute(
-                "class",
-                "section-two-list-elem card border-0"
-            );
-
-            let img = document.createElement("img");
-            img.setAttribute("class", "section-two-img card-img-top");
-            img.setAttribute("src", course.img);
-
-            let cardBody = document.createElement("div");
-            cardBody.setAttribute("class", "card-body");
-
-            let title = document.createElement("h3");
-            title.setAttribute("class", "section-two-content-header");
-            title.textContent = course.title;
-            title.style.height = "50px";
-
-            let desc = document.createElement("div");
-            desc.setAttribute("class", "section-two-content-desc");
-            desc.textContent = course.instructor;
-
-            let rating = document.createElement("div");
-            rating.setAttribute("class", "rating");
-            let ratingVal = document.createElement("span");
-            ratingVal.setAttribute("class", "rating-value");
-            ratingVal.innerHTML = "4.4";
-            rating.appendChild(ratingVal);
-            let starList = document.createElement("ul");
-            starList.setAttribute("class", "rating-list");
-            for (let i = 0; i < 5; i++) {
-                let star = document.createElement("li");
-                if (i === 4) {
-                    star.setAttribute("class", "fa fa-star-half-full checked");
-                } else {
-                    star.setAttribute("class", "fa fa-star checked");
-                }
-                starList.appendChild(star);
-            }
-            rating.appendChild(starList);
-
-            let newPrice = document.createElement("div");
-            newPrice.setAttribute("class", "section-two-price");
-            newPrice.textContent = course.newPrice;
-
-            let oldPrice = document.createElement("s");
-            oldPrice.setAttribute("class", "section-two-old-price");
-            oldPrice.textContent = course.oldPrice;
-
-            cardBody.appendChild(title);
-            cardBody.appendChild(desc);
-            cardBody.appendChild(rating);
-            cardBody.append(newPrice);
-            cardBody.append(oldPrice);
-
-            article.appendChild(img);
-            article.appendChild(cardBody);
-
-            courseList.appendChild(article);
-        }
-        topicContentTab.appendChild(courseList);
         topicContent.appendChild(topicContentTab);
     }
 };
 
-function renderPage(topic) {
-    let subHeader = document.querySelector(".section-two-sub-header");
-    let subParagraph = document.querySelector(".section-two-sub-paragraph");
-    subHeader.textContent = topic.content.header;
-    subParagraph.textContent = topic.content.paragraph;
-    renderList(topic.content.courses);
+function getActiveTab() {
+    let x = document.querySelector(".topic-list").querySelector(".active");
+    let number = x.id.substring(9, x.length);
+    return parseInt(number) - 1;
 }
 
-function renderList(courses) {
-    let parentDiv = document.querySelector(".section-two-list");
-    parentDiv.innerHTML = "";
+function search() {
+    let idx = getActiveTab();
+    let searchBar = document.querySelector(".search-bar").value;
+    let coursesList = document
+        .querySelector(".section-two-content")
+        .querySelector(".active")
+        .querySelector(".section-two-list");
+    let matchedCourses = [];
+    for (let course of data[idx].content.courses) {
+        let title = course.title;
+        if (title.includes(searchBar))
+            matchedCourses = [...matchedCourses, course];
+    }
+    courseIdxArr[idx] = 0;
+    renderList(matchedCourses, courseIdxArr[idx], coursesList);
+}
+
+function showNext() {
+    let idx = getActiveTab();
+
+    if (courseIdxArr[idx] + 1 < data[idx].content.courses.length) {
+        let coursesList = document
+            .querySelector(".section-two-content")
+            .querySelector(".active")
+            .querySelector(".section-two-list");
+        renderList(data[idx].content.courses, ++courseIdxArr[idx], coursesList);
+    }
+}
+function showBack() {
+    let idx = getActiveTab();
+
+    if (courseIdxArr[idx] != 0) {
+        let coursesList = document
+            .querySelector(".section-two-content")
+            .querySelector(".active")
+            .querySelector(".section-two-list");
+        renderList(data[idx].content.courses, --courseIdxArr[idx], coursesList);
+    }
+}
+
+getData("http://localhost:3000/topic");
+
+let searchBtn = document.querySelector(".search-btn");
+searchBtn.addEventListener("click", search);
+
+function renderPage(pageContnet, topicContentTab) {
+    let topic = document.createElement("div");
+    topic.setAttribute("class", "section-two-topic");
+
+    let subHeader = document.createElement("h2");
+    subHeader.setAttribute("class", "section-two-sub-header");
+    subHeader.textContent = pageContnet.content.header;
+    topic.appendChild(subHeader);
+
+    let subParagraph = document.createElement("p");
+    subParagraph.setAttribute("class", "section-two-sub-paragraph");
+    subParagraph.textContent = pageContnet.content.paragraph;
+    topic.appendChild(subParagraph);
+
+    topicContentTab.appendChild(topic);
+
+    let exploreBtn = document.createElement("input");
+    exploreBtn.setAttribute("class", "explore-btn");
+    exploreBtn.setAttribute("id", "explore-btn-" + pageContnet.id);
+    exploreBtn.setAttribute("type", "button");
+    exploreBtn.setAttribute("value", "Explore " + pageContnet.name);
+    topicContentTab.appendChild(exploreBtn);
+
+    let backBtn = document.createElement("input");
+    backBtn.setAttribute("type", "button");
+    backBtn.setAttribute("class", "back-btn");
+    backBtn.setAttribute("id", "back-btn-" + pageContnet.id);
+    backBtn.setAttribute("value", "back");
+    backBtn.addEventListener("click", showBack);
+    topicContentTab.appendChild(backBtn);
+
+    let nextBtn = document.createElement("input");
+    nextBtn.setAttribute("type", "button");
+    nextBtn.setAttribute("class", "next-btn");
+    nextBtn.setAttribute("id", "next-btn-" + pageContnet.id);
+    nextBtn.setAttribute("value", "next");
+    nextBtn.addEventListener("click", showNext);
+    topicContentTab.appendChild(nextBtn);
+
+    let courseList = document.createElement("div");
+    courseList.setAttribute("class", "section-two-list");
+    renderList(pageContnet.content.courses, 0, courseList);
+
+    topicContentTab.appendChild(courseList);
+}
+
+function renderList(courses, courseIdx, coursesList) {
+    coursesList.innerHTML = "";
     for (let i = 0; i < 5; i++) {
-        if (courseIdx + i >= courses.length) {
-            break;
-        }
-        let course = courses[courseIdx + i];
+        let course = courses[(courseIdx + i) % courses.length];
         let article = document.createElement("artice");
         article.setAttribute("class", "section-two-list-elem card border-0");
 
@@ -206,41 +206,6 @@ function renderList(courses) {
         article.appendChild(img);
         article.appendChild(cardBody);
 
-        parentDiv.appendChild(article);
+        coursesList.appendChild(article);
     }
 }
-
-function search() {
-    let searchBar = document.querySelector(".search-bar").value;
-    let matchedCourses = [];
-    for (let course of data.content.courses) {
-        let title = course.title;
-        if (title.includes(searchBar))
-            matchedCourses = [...matchedCourses, course];
-    }
-    courseIdx = 0;
-    renderList(matchedCourses);
-}
-function showNext() {
-    if (courseIdx + 5 < data.content.courses.length) {
-        courseIdx += 5;
-    }
-    renderList(data.content.courses);
-}
-function showBack() {
-    if (courseIdx != 0) {
-        courseIdx -= 5;
-    }
-    renderList(data.content.courses);
-}
-
-getData("http://localhost:3000/topic");
-
-let searchBtn = document.querySelector(".search-btn");
-searchBtn.addEventListener("click", search);
-
-// let nextBtn = document.querySelector(".next-btn");
-// nextBtn.addEventListener("click", showNext);
-
-// let backBtn = document.querySelector(".back-btn");
-// backBtn.addEventListener("click", showBack);
